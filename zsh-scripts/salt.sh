be18914089098f497b11330a1c41f290d0da6d4a
Matt Parlette <matthew.parlette@gmail.com>
2016-03-07 06:39:41 -0500

    Add timeout when away from network

--- zsh-scripts/salt.sh
+++ zsh-scripts/salt.sh
@@ -30,7 +30,7 @@ zbell_end() {
 
         if [[ "$duration" -gt 2 ]]; then
             # Redirect to /dev/null and disown (&!) so it doesn't pause the terminal while sending to salt
-            sudo salt-call event.send "cmd/$proc/return" process="$proc" duration="$duration" result="$result" > /dev/null 2>&1 &!
+            sudo salt-call --timeout=5 event.send "cmd/$proc/return" process="$proc" duration="$duration" result="$result" > /dev/null 2>&1 &!
         fi
 }
 
