a231cc804bebed91d5e33353a7919525fb70cd5c
Matt Parlette <matthew.parlette@gmail.com>
2016-07-12 14:47:43 -0400

    Converted cpu, hdd, log, and eye candy

---
+++ conky/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# First stop any running processes, then start conky
+
+# Kill any conky running
+killall -q conky
+# Kill any xwinwrap
+killall -q xwinwrap
+
+# Set wallpaper
+feh --bg-fill /home/matt/matthew.parlette@gmail.com/wallpaper/system/phoenix/dots-and-dashes.png
+
+# Start xwinwrap
+/home/matt/.conky/candy.sh /home/matt/.conky/quark.gif &
+# Start conky
+conky -c /home/matt/.conky/conky_cpus
+conky -c /home/matt/.conky/conky_hdd
+conky -c /home/matt/.conky/conky_loggs
