bd7efded1ce491226e6fd1329c5cd3b6cfd98ce0
Matt Parlette <matthew.parlette@gmail.com>
2016-07-14 10:51:35 -0400

    Added cpu history

--- conky/start.sh
+++ conky/start.sh
@@ -13,6 +13,7 @@ feh --bg-fill /home/matt/matthew.parlette@gmail.com/wallpaper/system/phoenix/dot
 /home/matt/.conky/candy.sh /home/matt/.conky/quark.gif &
 # Start conky
 conky -c /home/matt/.conky/conky_cpus
+conky -c /home/matt/.conky/conky_cpu_history
 conky -c /home/matt/.conky/conky_hdd
 conky -c /home/matt/.conky/conky_loggs
 conky -c /home/matt/.conky/conky_connections
